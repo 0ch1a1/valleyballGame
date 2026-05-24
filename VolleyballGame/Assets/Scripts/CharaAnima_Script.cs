7f40c73d6dedd78bc8c23dc7385e1cab63e12327
0ch1a1@DTK <taishi.842aka@icloud.com>
2026-05-24 22:27:43 +0900

    handAnimation

--- VolleyballGame/Assets/Scripts/CharaAnima_Script.cs
+++ VolleyballGame/Assets/Scripts/CharaAnima_Script.cs
@@ -18,6 +18,9 @@ public class CharaAnima_Script : MonoBehaviour
     private Vector3[] HandInitPos = new Vector3[2]; //Right: 0, Left :1
     private Vector3[] LegInitPos = new Vector3[2]; //Right: 0, Left :1
     private Vector3 HandTargetPos;
+    [SerializeField] private float HandMoveSpeed = 3f;
+    [SerializeField] private float HandHoldTime = 0.15f;
+    private float HandHoldElapsedTime;
 
     public bool HandAnimaStartFlag;
     private bool HandAnimaReturnFlag;
@@ -43,6 +46,8 @@ public class CharaAnima_Script : MonoBehaviour
         HandInitPos[1] = HandTrans[1].localPosition;
         LegInitPos[0] = LegTrans[0].localPosition;
         LegInitPos[1] = LegTrans[1].localPosition;
+        HandElapsedTime = 0f;
+        HandHoldElapsedTime = 0f;
 
         HandAnimaStartFlag = false;
         HandAnimaReturnFlag = false;
@@ -54,35 +59,49 @@ void HandAnimation(Transform RightTrans, Transform LeftTrans, Vector3 TargetPos)
 
         if (HandAnimaStartFlag)
         {
-            HandElapsedTime = 0;
+            HandElapsedTime = 0f;
+            HandHoldElapsedTime = 0f;
             HandTargetPos = TargetPos;
             HandAnimaMoveFlag = true;
             HandAnimaReturnFlag = false;
             HandAnimaStartFlag = false;
         }
 
-        if (HandAnimaMoveFlag)
+        if (!HandAnimaMoveFlag)
         {
-            if (HandElapsedTime > 1f)
-            {
-                HandAnimaReturnFlag = true;
-            }
-            if (HandElapsedTime < -0.2f)
-            {
-                HandAnimaMoveFlag = false;
-            }
+            return;
+        }
 
-            if (!HandAnimaReturnFlag)
+        if (!HandAnimaReturnFlag)
+        {
+            HandElapsedTime += Time.deltaTime * HandMoveSpeed;
+            float t = Mathf.Clamp01(HandElapsedTime);
+            RightTrans.localPosition = Vector3.Lerp(HandInitPos[0], HandTargetPos, t);
+            LeftTrans.localPosition = Vector3.Lerp(HandInitPos[1], HandTargetPos, t);
+
+            if (t >= 1f)
             {
-                HandElapsedTime += Time.deltaTime * 3f;
-                RightTrans.localPosition = Vector3.Lerp(HandInitPos[0], HandTargetPos, HandElapsedTime);
-                LeftTrans.localPosition = Vector3.Lerp(HandInitPos[1], HandTargetPos, HandElapsedTime);
+                HandHoldElapsedTime += Time.deltaTime;
+                if (HandHoldElapsedTime >= HandHoldTime)
+                {
+                    HandAnimaReturnFlag = true;
+                }
             }
-            else
+        }
+        else
+        {
+            HandElapsedTime -= Time.deltaTime * HandMoveSpeed;
+            float t = Mathf.Clamp01(HandElapsedTime);
+            RightTrans.localPosition = Vector3.Lerp(HandInitPos[0], HandTargetPos, t);
+            LeftTrans.localPosition = Vector3.Lerp(HandInitPos[1], HandTargetPos, t);
+
+            if (t <= 0f)
             {
-                HandElapsedTime -= Time.deltaTime * 3f;
-                RightTrans.localPosition = Vector3.Lerp(HandInitPos[0], HandTargetPos, HandElapsedTime);
-                LeftTrans.localPosition = Vector3.Lerp(HandInitPos[1], HandTargetPos, HandElapsedTime);
+                RightTrans.localPosition = HandInitPos[0];
+                LeftTrans.localPosition = HandInitPos[1];
+                HandAnimaMoveFlag = false;
+                HandAnimaReturnFlag = false;
+                HandHoldElapsedTime = 0f;
             }
         }
     }
